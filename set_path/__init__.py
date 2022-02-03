56d513568157e193ea1c7af61dd4306c71b581a1
Vasyl Kolomiiets <vasyl.i.kolomiets@gmail.com>
2022-02-03 10:25:46 +0200

    sebders.txt added

---
+++ set_path/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 19 22:35:57 2021
+
+@author: Vasil
+"""
+
+import sys
+
+sys.path.append(r"D:\OD\OneDrive\Projects\Chud_Amaz\Soft_in_dev\FEE_USA_2021")
+
+
+'''
+import importlib.util
+
+
+def module_from_file(module_name, file_path):
+    """Create module from file specified."""
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
+
+
+modules_path_name = r'D:\OneDrive\Projects\Chud_Amaz\Soft_in_dev\moduled_way\\'
+module_excel_formating_name = "excel_formatting"
+module_excel_formating_path = modules_path_name + "excel_formatting.py"
+
+module_from_file(module_excel_formating_name, module_excel_formating_path)
+
+
+module_data_processing_name = "data_processing"
+module_data_processing_path = modules_path_name + "data_processing.py"
+
+module_from_file(module_data_processing_name, module_data_processing_path)
+'''
